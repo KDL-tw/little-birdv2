a20097786f54ba7bc43ffd5249ff8be5dc038211
Kelyn Lanier <klanier@example.com>
2025-10-02 12:51:11 -0600

    Fix remaining ESLint errors
    - Re-add DialogDescription import that was accidentally removed
    - Add ESLint disable comment for empty interface in TextareaProps

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  // Additional props can be added here if needed
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
